6ae6d934498ad2a73db971a689f0468cbc997926
ElenaVedernikova <lenok-v@live.ru>
2021-06-11 12:39:52 +0300

    v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private int authorID;
+    private int postTime;
+    private String text;
+
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+
+    // + get/set на все поля
+}
