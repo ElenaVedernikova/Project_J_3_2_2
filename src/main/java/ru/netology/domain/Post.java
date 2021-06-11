8641ed028ac9431297607f2642ab15770c86121a
ElenaVedernikova <lenok-v@live.ru>
2021-06-11 14:04:27 +0300

    v2

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -3,12 +3,31 @@ package ru.netology.domain;
 public class Post {
     private int id;
     private int ownerId;
-    private int authorID;
-    private int postTime;
+    private int fromID;
+    private int createdBy;
+    private int date;
     private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String post_type;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
 
     private LikesInfo likesInfo;
     private CommentsInfo commentsInfo;
+    private Geo geo;
+    private ViewsService viewsService;
+    private RepostsInfo repostsInfo;
+    private Copyright copyright;
+    private PostSource postSource;
+    private Donut donut;
 
     // + get/set на все поля
 }
---
+++ src/main/java/ru/netology/domain/ViewsService.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+
+public class ViewsService {
+    private int count;
+
+    // + get/set на все поля
+}
